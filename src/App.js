349e259d6655abfd8097ef77f795181967cc35cd
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-11-23 17:10:25 -0800

    add Home component and background image (desktop)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import "./styles/main.scss";
+import Home from "./components/Home/Home";
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
