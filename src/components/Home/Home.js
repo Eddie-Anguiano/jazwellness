04a21f91b9a7803a9dd062d5ea4ffa7c2961719a
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-11-23 18:38:00 -0800

    add Hero component (desktop styles)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from "../Header/Header";
+import Hero from "../Hero/Hero";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="Home">
         <Header />
+        <Hero />
       </div>
     );
   }
