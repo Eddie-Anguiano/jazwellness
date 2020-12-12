0bbba8a843484ffab8cce7cc482ffaf6f98f4059
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-12-11 20:42:32 -0800

    convert Blog component to functional component utilizing hooks

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,19 +1,28 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import Load from "../Load/Load";
 import Client from "../../contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-export default class Blog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      blog: {},
-      load: false,
-    };
-  }
+export default function Blog(props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [blog, setBlogs] = useState({});
 
-  formatData = (response) => {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await Client.getEntry(props.match.params.id);
+        const formatedBlog = formatData(response);
+        setBlogs(formatedBlog);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, [props.match.params.id]);
+
+  const formatData = (response) => {
     const title = response.fields.title;
     const photo = response.fields.mainPicture.fields.file.url;
     const date = response.fields.date;
@@ -27,41 +36,21 @@ export default class Blog extends Component {
     };
   };
 
-  getData = async () => {
-    try {
-      this.setState({ load: true });
-      const response = await Client.getEntry(this.props.match.params.id);
-      const formatedBlog = this.formatData(response);
-      this.setState({ blog: formatedBlog });
-      this.setState({ load: false });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  return (
+    <>
+      {isLoading && <Load />}
+      <Header />
+      <div className="Blog wrapper">
+        <section className="Blog-post">
+          <div className="Blog-date">{blog.date}</div>
+          <h1 className="Blog-title">{blog.title}</h1>
+          <div className="Blog-author">by Jasmin Chavez</div>
 
-  componentDidMount() {
-    this.getData();
-  }
-
-  render() {
-    console.log(this.state.blog.jsx);
-    return (
-      <>
-        {this.state.load && <Load />}
-
-        <Header />
-        <div className="Blog wrapper">
-          <section className="Blog-post">
-            <div className="Blog-date">{this.state.blog.date}</div>
-            <h1 className="Blog-title">{this.state.blog.title}</h1>
-            <div className="Blog-author">by Jasmin Chavez</div>
-
-            <img src={this.state.blog.photo} alt="" className="Blog-img" />
-            <div className="Blog-richText">{this.state.blog.jsx}</div>
-          </section>
-        </div>
-        <footer className="Blog-footer">jazwellness.com&#174;</footer>
-      </>
-    );
-  }
+          <img src={blog.photo} alt="" className="Blog-img" />
+          <div className="Blog-richText">{blog.jsx}</div>
+        </section>
+      </div>
+      <footer className="Blog-footer">jazwellness.com&#174;</footer>
+    </>
+  );
 }
