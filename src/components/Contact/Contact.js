4caf7bea5c896e7f77e214464d33b12c58d86383
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-12-13 17:31:31 -0800

    connect contact form to netlify

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import card from "../images/card.jpg";
 import { GrFacebook } from "react-icons/gr";
 import { GrInstagram } from "react-icons/gr";
 import { IconContext } from "react-icons";
@@ -10,7 +9,6 @@ export default function Contact() {
     <section className="contact">
       <div className="contact-wrapper wrapper">
         <div className="contact-info">
-          {/* <img src={card} alt="" className="contact-img" /> */}
           <div className="contact-social-container">
             <p className="contact-header">Contact Me!</p>
             <div className="contact-email">jasminchavez@gmail.com</div>
@@ -27,19 +25,33 @@ export default function Contact() {
           </div>
         </div>
 
-        <form className="contact-form">
+        <form
+          className="contact-form"
+          name="contact"
+          method="POST"
+          data-netlify="true">
           <div className="contact-name-container">
             <div className="contact-first-name">
               <label htmlFor="first_name" className="contact-label">
                 First Name
               </label>
-              <input id="first_name" type="text" className="contact-input" />
+              <input
+                id="first_name"
+                name="first_name"
+                type="text"
+                className="contact-input"
+              />
             </div>
             <div className="contact-last_name">
               <label htmlFor="last_name" className="contact-label">
                 last Name
               </label>
-              <input id="last_name" type="text" className="contact-input" />
+              <input
+                name="last_name"
+                id="last_name"
+                type="text"
+                className="contact-input"
+              />
             </div>
             <div className="contact-last_name"></div>
           </div>
@@ -47,7 +59,12 @@ export default function Contact() {
           <label htmlFor="email" className="contact-label">
             Email
           </label>
-          <input id="email" type="text" className="contact-input" />
+          <input
+            id="email"
+            name="email"
+            type="email"
+            className="contact-input"
+          />
 
           <label htmlFor="message" className="contact-label">
             Message
@@ -56,9 +73,10 @@ export default function Contact() {
             id="message"
             type="text"
             className="contact-input contact-textarea"
+            name="text_area"
           />
 
-          <button className="contact-button">
+          <button type="submit" className="contact-button">
             <MdSend />
             <span className="contact-button-text">Submit</span>
           </button>
